1e9afc737a5900c28f9d0d7c8b98bb0a3057c97f
android <trevd1234@gmail.com>
2014-03-15 01:06:13 +0000

    meh!

--- android_tests/ped_unit_examples.c
+++ android_tests/ped_unit_examples.c
@@ -2,7 +2,7 @@
 #include <parted/parted.h>
 int main (int argc, char* argv[])
 {
-        char* size;
+        
         PedDevice* device;
         
           
@@ -14,9 +14,23 @@ int main (int argc, char* argv[])
         device = ped_device_get (argv[1]);
         if (device == NULL) goto error;
         
+        // Get the default unit
         PedUnit unit = ped_unit_get_default();
         const char* unit_name = ped_unit_get_name(unit);
         printf("Default Unit Name = %s ( %d )\n",unit_name,unit);
+        //free((void*)unit_name);
+        
+        ped_unit_set_default(PED_UNIT_MEGABYTE);
+        
+        unit = ped_unit_get_default();
+        const char* new_unit_name = ped_unit_get_name(unit);
+        printf("New Default Unit Name = %s ( %d )\n",new_unit_name,unit);
+        //free(new_unit_name);
+        
+        char* size = ped_unit_format_byte (device, device->length * device->sector_size);
+        if (size == NULL) goto error;
+        printf("Size: %s\n", size);
+        
         ped_device_destroy(device);
         
         return 0;
