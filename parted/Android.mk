928fbf96739d612e9ce9a775986635817036f004
android <trevd1234@gmail.com>
2014-04-04 22:07:39 +0100

    Add Static Build

--- parted/Android.mk
+++ parted/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
+parted_src_files := \
 	command.c	\
 	parted.c	\
 	strlist.c	\
@@ -9,10 +9,24 @@ LOCAL_SRC_FILES := \
 	table.c		\
 	version.c	\
 
-LOCAL_CFLAGS :=
+
+parted_static_libraries := libparted libgnulib libparted-fs libuuid
+parted_c_includes := $(LOCAL_PATH)/../include $(LOCAL_PATH)/../lib
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(parted_src_files)
 LOCAL_MODULE := parted
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := libparted libgnulib libparted-fs libuuid
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include $(LOCAL_PATH)/../lib
+LOCAL_STATIC_LIBRARIES := $(parted_static_libraries)
+LOCAL_C_INCLUDES := $(parted_c_includes)
 
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(parted_src_files)
+LOCAL_MODULE := parted-static
+LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_LIBRARIES := $(parted_static_libraries) libc
+LOCAL_C_INCLUDES := $(parted_c_includes)
+LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
